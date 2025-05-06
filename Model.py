6641de90143c9315953e08f4663a1f60b0e2854f
Julia11990 <uliazhdanova071@gmail.com>
2025-05-06 15:08:38 +0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,7 +1,16 @@
-levels = ["1", "2"]
+#Model
+levels = ["1"]
+
+actions ["1-выбрать уровень", "2-начать игру", "0-выйти"]
 
 def add_levels(title):
     levels.append(title)
+    
+def add_actions(title):
+    actions.append(title)
 
 def get_levels():
     return levels
+    
+def get_actions():
+    return actions

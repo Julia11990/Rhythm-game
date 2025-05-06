bf1bac0c313e700ca45755b139242ae75f3ec9a0
Julia11990 <uliazhdanova071@gmail.com>
2025-05-06 15:07:22 +0700

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -1,14 +1,30 @@
+#Contrller
 from Model import *
 from View import *
+from Level1 import *
 
-def main():
-    choice = display_menu()
-    print(choice)
-    if choice == "1":
-        levels_list = get_levels()
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Меню"
+    input_fild = ft.TextFeld(label = "Введи номер действия")
+    ouput_text = ft.Text()
+    
+    def on_button_click(e):
+        output_text.value = f"Вы выбрали: { input_field.value}"
+        page.updaete() 
+        
+    choice = output_text 
+     if choice == "1":
+        levels_list = levels_menu()
         show_levels(levels_list)
     elif choice =="2":
-        print("3...")
-        print("2...")
-        print("1...")
-        print("start")
+        level1 =  _init_(self)
+        show_level1(level1)
+    elif choice == "0":
+        break
+    else:
+        print("Такого действия нет")
+    page.add(input_field, button, output_text)
+    label = ft.Text(actions)
+ft.app(target=main)

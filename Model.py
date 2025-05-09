7d0634c420f53b7ca3c52a9fdeb0b0a83a518c68
Julia11990 <uliazhdanova071@gmail.com>
2025-05-09 15:35:27 +0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -3,28 +3,29 @@ import random
 
 class Note:
     def __init__(self, x, y, speed):
-        self.x = x
-        self.y = y
-        self.speed = speed
-        self.radius = 30
+        self.x = x  # Позиция по оси X
+        self.y = y  # Позиция по оси Y
+        self.speed = speed  # Скорость падения
+        self.radius = 30  # Радиус ноты
 
     def move(self):
-        self.y += self.speed
+        self.y += self.speed  # Перемещение вниз по Y
 
     def draw(self, surface):
+        # Рисование ноты 
         pygame.draw.circle(surface, (255, 255, 0), (self.x, int(self.y)), self.radius)
 
 
 class Bonus:
     def __init__(self, x, y, speed):
-        self.x = x
-        self.y = y
-        self.speed = speed
-        self.radius = 20
+        self.x = x  # Позиция по оси X
+        self.y = y  # Позиция по оси Y
+        self.speed = speed  # Скорость падения
+        self.radius = 20  # Радиус бонуса
 
     def move(self):
-        self.y += self.speed
+        self.y += self.speed  # Перемещение вниз по Y
 
     def draw(self, surface):
+        # Рисование бонуса
         pygame.draw.circle(surface, (0, 255, 0), (self.x, int(self.y)), self.radius)
-

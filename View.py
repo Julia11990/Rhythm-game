434bfa36c060a6aa559ad127862595fece321c52
Julia11990 <uliazhdanova071@gmail.com>
2025-05-12 17:42:44 +0700

    Update View.py

--- View.py
+++ View.py
@@ -1,19 +1,23 @@
 #View
 import pygame
+from model import width, height
 
 class Interface:
-    def __init__(self):
-        self.font = pygame.font.SysFont(None, 36)  # Отображение текста
+    def init(self):
+        self.font = pygame.font.SysFont(None, 36)
 
     def draw_score(self, surface, score):
-        # Отображение счета на экране
-        score_text = self.font.render(f'Счет: {score}', True, (255, 255, 255))
-        surface.blit(score_text, (10, 10))  # Позиция текста
+        text = self.font.render(f'Счет: {score}', True, (255, 255, 255))
+        surface.blit(text, (10, 10))
 
     def draw_game_over(self, surface):
-        # Отображение сообщения об окончании игры
         game_over_text = self.font.render('Игра окончена!', True, (255, 0, 0))
         restart_text = self.font.render('Нажмите R, чтобы перезапустить', True, (255, 255, 255))
-        surface.blit(game_over_text, (width // 2 - 100, height // 2 - 20))  # Центрирование текста
+        surface.blit(game_over_text, (width // 2 - 100, height // 2 - 20))
         surface.blit(restart_text, (width // 2 - 200, height // 2 + 20))
 
+def draw_note(surface, note):
+    pygame.draw.circle(surface, (255, 255, 0), (note.x, int(note.y)), note.radius)
+
+def draw_bonus(surface, bonus):
+    pygame.draw.circle(surface, (0, 255, 0), (bonus.x, int(bonus.y)), bonus.radius)
